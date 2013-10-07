32db3c5bd5f71f51512e0cf4be2da2fcd1a5b993
Jeffrey Horn <jrhorn424@gmail.com>
2013-10-07 03:10:40 -0400

    ICES customizations package (2013-10-22 16:45)

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :shared_paths, [
 
 set :deploy_to, '/u/apps/recruiter'
 set :repository, 'git@github.com:jrhorn424/recruiter.git'
-set :branch, 'master'
+set :branch, 'master-ices'
 set :app_root, File.expand_path('../../', __FILE__)
 set :application, 'recruiter'
 set :rails_env, 'production'
@@ -62,7 +62,8 @@ end
 
 desc "Upload secret configuration files."
 task :config => :environment do
-  scp_upload "#{app_root}/config/{application,database}.yml", "#{deploy_to}/shared/config/", verbose: true
+  scp_upload "#{app_root}/config/application.yml.ices", "#{deploy_to}/shared/config/application.yml", verbose: true
+  scp_upload "#{app_root}/config/database.yml.ices", "#{deploy_to}/shared/config/database.yml", verbose: true
 end
 
 namespace :bower do
